e1553858d75b3128fdb11de7bc2dad927a5fe25a
pattui <pattyzul@users.noreply.github.com.>
2025-07-23 09:56:44 +0700

    fix workshop

---
+++ class03_nestedif.py
@@ -0,0 +1,15 @@
+user = input()
+password = input("password:")
+
+if user == "admin":
+    if password == "admin123":
+        print("you are admin")
+    else:
+        print("wrong")
+elif x == "user":
+    if y == "user123":
+        print("you are user")
+    else:
+        print("wrong")
+else:
+    print("not found")            
