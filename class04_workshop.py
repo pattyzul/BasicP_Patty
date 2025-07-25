6a3fdacf024d8f9501357d1dcda6f43bae64ad69
Student <Student@Train11-4-PC056.control.ad.sit.kmutt.ac.th>
2025-07-25 09:17:27 +0700

    fix

--- class04_workshop.py
+++ class04_workshop.py
@@ -1,12 +1,14 @@
-# # ฟังก์ชันแสดงรายชื่อหนังทั้งหมดในระบบ
-# # TODO: วนลูปแสดงชื่อหนังพร้อมราคาตั๋ว
-# def show_movies(movie_list):
-#     for movie in movie_list:
-#         print(movie["movie_name"], movie["ticket_price"])
-# # ฟังก์ชันตรวจสอบอายุตามข้อจำกัดของหนัง
-# def check_age(user_age, age_restriction):
-#     if(age_restriction == G):
-#         print("")
+# ฟังก์ชันแสดงรายชื่อหนังทั้งหมดในระบบ
+# TODO: วนลูปแสดงชื่อหนังพร้อมราคาตั๋ว
+def show_movies(movie_list):
+    for movie in movie_list:
+        print(movie["movie_name"], movie["ticket_price"])
+# ฟังก์ชันตรวจสอบอายุตามข้อจำกัดของหนัง
+def check_age(user_age, age_restriction):
+    if(age_restriction == "G"):
+        print("Doo dai")
+    elif(user_age < age_restriction):
+        print("Doo mai dai :(")
 
 
 #     # TODO: ถ้า age_restriction เป็น 'G' ให้ผ่านเลย
@@ -18,14 +20,14 @@
 #     # ถ้าไม่ใช่ คืนราคาเดิม
  
 # # ฟังก์ชันสำหรับการซื้อบัตรชมหนัง
-# # def buy_ticket(movie_list):
-#      show_movies(movie_list)
-#      choose = int(input("choose movie type: "))
-#      age = int(input("your age: "))
-#      check_age(age,movie_list[choose]["age_restriction"])
+def buy_ticket(movie_list):
+     show_movies(movie_list)
+     choose = int(input("choose movie name: "))
+     age = int(input("your age: "))
+     check_age(age,movie_list[choose-1]["age_restriction"])
 
 
-#     # TODO:
+    # # TODO:
     # 1. เรียก show_movies เพื่อแสดงรายชื่อหนัง
     # 2. รับค่าตัวเลือกหนังจากผู้ใช้ (1-5)
     # 3. รับอายุผู้ใช้
@@ -35,37 +37,39 @@
     # 6. คำนวณราคาตั๋วโดยใช้ calculate_price
     # 7. แสดงผลการซื้อบัตร พร้อมชื่อหนัง, เสียงที่เลือก, ราคาตั๋ว
  
-# # def main():
-# #     # TODO: สร้างรายการหนังเป็น list ของ dict โดยเก็บข้อมูล movie_name, ticket_price, genre, age_restriction
-# #     movies = [
-# #         {'movie_name': 'Avengers Endgame', 'ticket_price': 300, 'genre': 'Action', 'age_restriction': '13'},
-# #         {'movie_name': 'Inception', 'ticket_price': 280, 'genre': 'Sci-Fi', 'age_restriction': '13'},
-# #         {'movie_name': 'It', 'ticket_price': 180, 'genre': 'Horror', 'age_restriction': '18'},
-# #         {'movie_name': 'The Notebook', 'ticket_price': 250, 'genre': 'Romantic', 'age_restriction': '13'},
-# #         {'movie_name': 'Harry Potter and the Sorcerer\'s Stone', 'ticket_price': 260, 'genre': 'Fantasy', 'age_restriction': 'G'}
-#     ]
+def main():
+    # TODO: สร้างรายการหนังเป็น list ของ dict โดยเก็บข้อมูล movie_name, ticket_price, genre, age_restriction
+    movies = [
+        {'movie_name': 'Avengers Endgame', 'ticket_price': 300, 'genre': 'Action', 'age_restriction': '13'},
+        {'movie_name': 'Inception', 'ticket_price': 280, 'genre': 'Sci-Fi', 'age_restriction': '13'},
+        {'movie_name': 'It', 'ticket_price': 180, 'genre': 'Horror', 'age_restriction': '18'},
+        {'movie_name': 'The Notebook', 'ticket_price': 250, 'genre': 'Romantic', 'age_restriction': '13'},
+        {'movie_name': 'Harry Potter and the Sorcerer\'s Stone', 'ticket_price': 260, 'genre': 'Fantasy', 'age_restriction': 'G'}
+    ]
  
-#     # TODO: แสดงเมนูให้ผู้ใช้เลือก
-#     # 1. แสดงหนังทั้งหมด
-#     # 2. ซื้อตั๋วหนัง
-#     print("show all movies[1]: ")
-#     print("show tickets   [2]: ")
+    # TODO: แสดงเมนูให้ผู้ใช้เลือก
+    # 1. แสดงหนังทั้งหมด
+    # 2. ซื้อตั๋วหนัง
+    print("show all movies[1]: ")
+    print("show tickets   [2]: ")
  
-#     # รับค่าตัวเลือกเมนูจากผู้ใช้
-#     menu = input("เลือกเมนู: ")
-#     if (menu == "1"):
-#         show_movies(movies)
+    # รับค่าตัวเลือกเมนูจากผู้ใช้
+    menu = input("เลือกเมนู: ")
+    if (menu == "1"):
+        show_movies(movies)
 
-#     elif(menu == "2"):
-#         buy_ticket(movies)
+    elif(menu == "2"):
+        buy_ticket(movies)
+    else:
+        print("Mai took tong")
         
 
  
-#     # TODO: ตรวจสอบเมนูที่เลือก
-#     # ถ้าเลือก 1 ให้เรียก show_movies พร้อมส่ง movies
-#     # ถ้าเลือก 2 ให้เรียก buy_ticket พร้อมส่ง movies
-#     # ถ้าเลือกอื่น ให้แสดงข้อความว่าเมนูไม่ถูกต้อง
+    # TODO: ตรวจสอบเมนูที่เลือก
+    # ถ้าเลือก 1 ให้เรียก show_movies พร้อมส่ง movies
+    # ถ้าเลือก 2 ให้เรียก buy_ticket พร้อมส่ง movies
+    # ถ้าเลือกอื่น ให้แสดงข้อความว่าเมนูไม่ถูกต้อง
  
-# # เรียก main เพื่อเริ่มโปรแกรม
-# main()
+# เรียก main เพื่อเริ่มโปรแกรม
+main()
  
